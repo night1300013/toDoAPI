c1a7028b2e0e096b9a2d2e08b4dcac93170502ce
ShangMing Cherng <night1300013@gmail.com>
2018-04-06 21:04:17 -0700

    add item destroy and item/user/list update

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::ItemsController < ApiController
   before_action :require_login!
-  
+
   def create
     list = List.find(params[:list_id])
     item = list.items.build(item_params)
@@ -9,6 +9,25 @@ class Api::ItemsController < ApiController
       render json: item
     else
       render json: {errors: item.errors.full_messages}, status: :unprocessable_entry
+    end
+  end
+
+  def destroy
+    begin
+      item = Item.find(params[:id])
+      item.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render json: {}, status: :not_found
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: item
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -25,6 +25,15 @@ class Api::UsersController < ApiController
     end
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
